0a57d99f3c1c860975463ffd9f28fc1bbf252a2e
stormy <raphael.beltrand@gmail.com>
2019-11-21 11:01:00 +0100

    dfog

--- db/schema.rb
+++ db/schema.rb
@@ -136,4 +136,5 @@ ActiveRecord::Schema.define(version: 2019_11_06_094206) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
